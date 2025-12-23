b78be7bbfe9c32f986ab76ad0be7637f47fc5a4e
dntrnk <dantronilluminatormusic@gmail.com>
2025-12-23 18:50:25 +0700

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,11 +1,8 @@
-require("src/lib/NFHSound")
-
-require("src/objects/neighbour")
 require("src/objects/woody")
 
 cameraX, cameraY = 0, 0
 
-nextScene = "src/scenes/menu/madebydntrnk.lua"
+nextScene = "src/scenes/levels/s0ep1.lua"
 
 while true do
     dofile(nextScene)
